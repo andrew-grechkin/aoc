8644fbfa0035bb3e52285b0ea4036fd4b720f8a8
Andrew Grechkin <andrew.grechkin@gmail.com>
2023-12-07 08:50:44 +0100

    2023 d1

--- 2022/day1.cjs
+++ 2022/day1.cjs
@@ -2,20 +2,28 @@
 
 const fs = require('fs')
 
+/**
+ * Sum of all elements in array
+ * @param {Array<number>} arr - array of integers
+ * @returns {number} - sum
+ * @example
+ * const arr = [1,2,3,4,5]
+ *
+ * const result = sum(arr);
+ * console.log(result);
+ * // Logs: 15
+ */
+function sum(arr) {
+    return arr.reduce((acc, val) => {
+        return acc + val
+    }, 0)
+}
+const to_elf = (str) => str.split('\n').map((num) => parseInt(num))
+
 const data = fs.readFileSync('2022/day1', 'utf8').trim()
 const paragraphs = data.split('\n\n')
-const elves = paragraphs.map((str) => str.split('\n').map((num) => parseInt(num)))
-const calories = elves
-    .map((arr) =>
-        arr.reduce((acc, val) => {
-            return acc + val
-        }, 0),
-    )
-    .sort()
+const elves = paragraphs.map(to_elf)
+const calories = elves.map((it) => sum(it)).sort()
 
 console.log(calories[calories.length - 1])
-console.log(
-    calories.slice(-3).reduce((acc, val) => {
-        return acc + val
-    }, 0),
-)
+console.log(sum(calories.slice(-3)))
---
+++ 2023/day1.pl
@@ -0,0 +1,49 @@
+#!/usr/bin/env perl
+
+use v5.38;
+use autodie;
+use open ':locale';
+use utf8;
+use warnings     qw(FATAL utf8);
+use experimental qw(builtin class declared_refs defer for_list refaliasing try);
+
+use List::Util qw(sum0);
+use Mojo::File qw(path);
+use Data::Printer;
+
+sub string_to_number1 ($str) {
+    state $first = qr{^ .*? (\d)}x;
+    state $last  = qr{^ .*  (\d)}x;
+    my @result = (($str =~ $first), ($str =~ $last));
+    return join '', @result;
+}
+
+sub string_to_number2 ($str) {
+    my %nums = (
+        one   => 1,
+        two   => 2,
+        three => 3,
+        four  => 4,
+        five  => 5,
+        six   => 6,
+        seven => 7,
+        eight => 8,
+        nine  => 9,
+    );
+    state $or    = join '|', keys %nums;
+    state $first = qr{^ .*? (\d|$or)}x;
+    state $last  = qr{^ .*  (\d|$or)}x;
+    my @result = (($str =~ $first), ($str =~ $last));
+    @result = map {exists $nums{$_} ? $nums{$_} : $_} @result;
+    return join '', @result;
+}
+
+my $data     = path('2023/day1')->slurp;
+my @lines    = split m/\R/, $data;
+my @numbers1 = map {string_to_number1($_)} @lines;
+my @numbers2 = map {string_to_number2($_)} @lines;
+
+say {*STDOUT} sum0 @numbers1;
+say {*STDOUT} sum0 @numbers2;
+
+__END__
